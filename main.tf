d4c48c4fb7ec8948acc136b1a94a44b1de9d208e
Explorer-26 <amrita.cys.dt@gmail.com>
2026-07-11 14:56:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
+
+resource "local_file" "hello" {
+  filename = "hello.txt"
+  content  = "Hello Terraform, this is smaple test1"
+}
+
+resource "local_file" "file1" {
+  filename = "file1.txt"
+  content  = "Hello"
+}
+
+resource "local_file" "file2" {
+  filename = "file2.txt"
+  content  = local_file.file1.content
+}
